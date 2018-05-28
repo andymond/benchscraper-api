bc1adb113b7e251b642eab19b0011755ae24c113
Andrew Dymond <andy.dymond@gmail.com>
2018-05-28 13:55:37 -0600

    Update /items route to /api/v1/items route in app.js

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/items', itemsRouter);
+app.use('/api/v1/items', itemsRouter);
 
 module.exports = app;
