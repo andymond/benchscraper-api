2fd892278750a578af599f40d7996e4d95ff5a02
Andrew Dymond <andy.dymond@gmail.com>
2018-05-28 15:18:33 -0600

    Add returns items with matching name case insensitive test

--- test/api/v1/test-items-endpoints.js
+++ test/api/v1/test-items-endpoints.js
@@ -71,5 +71,22 @@ describe('/api/v1/items endpoints', function() {
         response.body[1].seller.should.eq('Sams Club')
       })
     })
+
+    it('returns items with matching name case insensitive', () => {
+      return chai.request(app)
+      .get('/api/v1/items')
+      .query({name: 'DONUTs'})
+      .then((response) => {
+        response.should.have.status(200)
+        response.body.should.be.an('array')
+        response.body.length.should.eq(2)
+        response.body[0].name.should.eq('donuts')
+        response.body[1].name.should.eq('donuts')
+        response.body[0].price.should.eq('$1.30')
+        response.body[1].price.should.eq('$1.65')
+        response.body[0].seller.should.eq('Costco')
+        response.body[1].seller.should.eq('Sams Club')
+      })
+    })
   })
 })
