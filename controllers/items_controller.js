e277c0b7ff3139edae6d35ae36fb893a7312735d
Andrew Dymond <andy.dymond@gmail.com>
2018-05-28 15:21:17 -0600

    Add regex processing for query string params in items controller

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -3,6 +3,10 @@ pry = require("pryjs")
 
 module.exports = class ItemsController {
   static index(req, res, next) {
+    for (var key in req.query) {
+      req.query[key] = new RegExp(req.query[key], 'i')
+    }
+
     Item.find(req.query).sort({'_id': 1}).then((items) => {
       res.json(items)
     })
