02b15e54d876c975a0196ce8a7a099d2e80e8eb1
Andrew Dymond <andy.dymond@gmail.com>
2018-05-29 13:29:38 -0600

    Add seeds directory & add seeds.js config file to .gitignore

---
+++ seeds/items.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    "name": "apple",
+    "price": "$1",
+    "seller": "walmart"
+  },
+  {
+    "name": "apple",
+    "price": "$.75",
+    "seller": "target"
+  },
+  {
+    "name": "apple",
+    "price": "$.50",
+    "seller": "costco"
+  },
+  {
+    "name": "orange",
+    "price": "$1",
+    "seller": "walmart"
+  },
+  {
+    "name": "orange",
+    "price": "$1",
+    "seller": "target"
+  },
+  {
+    "name": "orange",
+    "price": "$1",
+    "seller": "costco"
+  }
+]
